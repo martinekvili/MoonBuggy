09a9bfe1674802972c059843de15dba35b46af25
Martinek Vilmos <vilmos.martinek@gmail.com>
2014-04-26 10:54:49 +0200

    Added main MIDlet

---
+++ src/top/MoonBuggy.java
@@ -0,0 +1,22 @@
+package top;
+
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+public class MoonBuggy extends MIDlet {
+	
+	protected void startApp() throws MIDletStateChangeException {
+
+	}
+
+	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void pauseApp() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
